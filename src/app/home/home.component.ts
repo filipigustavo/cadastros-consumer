32863214b930f6c3051c69051a5ee3f6da0f8aa0
GRPCOM <grpcom@311PLAT046382D.local>
2017-06-14 18:40:14 -0300

    post

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../services/home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [HomeService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private home:HomeService) { }
+
+  fieldResponse:string;
 
   ngOnInit() {
+  }
+
+  enviarPost(field:string):void{
+    this.home.postField();
   }
 
 }
